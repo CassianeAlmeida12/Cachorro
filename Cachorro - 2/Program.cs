a2bf42694f9875eb73816120df3665c818c828a0
Cassiane Almeida <cassiane.almeida@estudante.ifro.edu.br>
2023-06-12 16:09:07 -0400

    first commit

---
+++ Cachorro - 2/Program.cs
@@ -0,0 +1,11 @@
+﻿Cachorro brutus = new Cachorro();
+brutus.nome = "Brutus";
+brutus.raca = "PitBull";
+string b = "AuAu";
+brutus.Latir(b);// é um metodo
+
+Cachorro max = new Cachorro();
+max.nome = "Max";
+max.raca = "vira lata";
+string c = "AuAuuuuuu";
+max.Latir(c);
---
+++ Cachorro - 2/cachorro.cs
@@ -0,0 +1,11 @@
+﻿public class Cachorro
+{
+    public string nome;
+    public string raca;
+
+    public void Latir(string barulho/*é um parametro(você pode passar quantos parametros forem necessarios)*/) 
+    {
+        Console.WriteLine("O cachorro " + nome + " é da raça " + raca + " e faz "  + barulho);
+    }
+
+}
